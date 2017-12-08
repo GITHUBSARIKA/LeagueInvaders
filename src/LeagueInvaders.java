db0cf897374f17eb91985f1ac533fe5439cd7cba
TJHL <league@iMac.attlocal.net>
2017-12-07 18:26:56 -0800

    Part 5

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
@@ -5,11 +7,12 @@ public class LeagueInvaders {
 	JFrame frame;
 	final int width = 500;
 	final int height = 800;
+	GamePanel gamepanel=new GamePanel();
 
 	LeagueInvaders() {
 		frame = new JFrame();
 		frame.setSize(width, height);
-		frame.setTitle("LeagueInvaders");
+		frame.setTitle("LeagueInvaders");frame.addKeyListener(gamepanel);
 	}
 
 	public static void main(String[] args) {
@@ -19,8 +22,11 @@ public class LeagueInvaders {
 	}
 
 	void setup() {
+		frame.add(gamepanel);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+		gamepanel.startGame();
 
 	}
 }
